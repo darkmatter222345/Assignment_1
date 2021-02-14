c001188f8dea51194d58970be85572bcfd4a6a1b
oseile <oseile@sheridancollege.ca>
2021-02-14 01:11:03 -0500

    Created a For loop, 7 arrays, and added random calculations.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,5 +1,7 @@
 
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
+import java.util.Random;
+ 
 
 /**
  *
@@ -10,6 +12,21 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
+        Card[] TheHand = new Card[7];
+        
+          for (int i=0; i<TheHand.length; i++)
+        {
+            Card a = new Card();
+            
+            a.setValue((int)((Math.random()*13)+1));
+            a.setSuit(Card.SUITS[(int)((Math.random()*4)+0)]);
+            
+           
+      
+            
+            TheHand[i] = a;
+
+        }
        
     }
     
